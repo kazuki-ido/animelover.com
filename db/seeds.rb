0548c3d578f6780f887db1cf0035ba2ba9b2a990
kazuki ido <0sn58m19324338p@gmail.com>
2021-08-16 10:25:55 +0000

    seedにユーザーのダミー情報を書く

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  name = Faker::Name.name
+  email = Faker::Internet.unique.free_email
+  password = Faker::Internet.password(min_length: 6)
+
+  User.create(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
