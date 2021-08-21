5fedeefa0317478902966417762c602e952bb73f
kazuki ido <0sn58m19324338p@gmail.com>
2021-08-21 00:02:28 +0000

    seedのユーザーデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-50.times do
+10.times do
   name = Faker::Name.name
   email = Faker::Internet.unique.free_email
   password = Faker::Internet.password(min_length: 6)
 
-  User.create(
+  User.create!(
     name: name,
     email: email,
     password: password,
